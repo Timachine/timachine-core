1c5df5cf43aeee54cab8313b1df71911c1397077
Liu Yang <liuyang@worksap.co.jp>
2014-11-06 17:42:11 +0800

    Fix other migrations.

--- timachine-dynamo/src/test/java/jp/co/worksap/timachine/dynamo/migrations/M20141107173530.java
+++ timachine-dynamo/src/test/java/jp/co/worksap/timachine/dynamo/migrations/M20141107173530.java
@@ -2,12 +2,14 @@ package jp.co.worksap.timachine.dynamo.migrations;
 
 import jp.co.worksap.dynamo.DynamoUtil;
 import jp.co.worksap.timachine.dynamo.DynamoMigration;
-import jp.co.worksap.timachine.model.Revocable;
+import jp.co.worksap.timachine.model.Down;
+import jp.co.worksap.timachine.model.Migration;
+import jp.co.worksap.timachine.model.Up;
 
-@Revocable
-public class M2 extends DynamoMigration {
+@Migration
+public class M20141107173530 extends DynamoMigration {
 
-    @Override
+    @Up
     public void up() {
         dynamo().createTable("ctmapp", DynamoUtil.createThroughput(1L, 1L),
                 DynamoUtil.createAttrList("appId", "S", "accountId", "S"),
@@ -15,7 +17,7 @@ public class M2 extends DynamoMigration {
         System.out.println("M2 Dynamo up");
     }
 
-    @Override
+    @Down
     public void down() {
         dynamo().deleteTable("ctmapp");
     }
