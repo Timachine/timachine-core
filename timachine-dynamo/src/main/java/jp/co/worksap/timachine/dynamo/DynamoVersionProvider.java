31061a75ae326bee842543deecb3953245de81b1
Liu Yang <liuyang@worksap.co.jp>
2014-11-07 11:22:16 +0800

    Finish migrate goal.

---
+++ timachine-dynamo/src/main/java/jp/co/worksap/timachine/dynamo/DynamoVersionProvider.java
@@ -0,0 +1,14 @@
+package jp.co.worksap.timachine.dynamo;
+
+import jp.co.worksap.timachine.spi.VersionProvider;
+import sun.misc.Version;
+
+/**
+ * Created by liuyang on 14-11-7.
+ */
+public class DynamoVersionProvider implements VersionProvider {
+    @Override
+    public String currentVersion() {
+        return null;
+    }
+}
--- timachine-maven-plugin/src/main/java/MigrateMojo.java
+++ timachine-maven-plugin/src/main/java/MigrateMojo.java
@@ -1,24 +1,22 @@
+import jp.co.worksap.timachine.Executor;
 import jp.co.worksap.timachine.model.Migration;
+import jp.co.worksap.timachine.model.Options;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.plugins.annotations.*;
 import org.reflections.Reflections;
-import org.reflections.util.ConfigurationBuilder;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by liuyang on 14-11-7.
  */
-@Mojo(name = "migrate",requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+@Mojo(name = "migrate", requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+@Execute(phase = LifecyclePhase.COMPILE)
 public class MigrateMojo extends AbstractMojo {
 
 
@@ -27,22 +25,48 @@ public class MigrateMojo extends AbstractMojo {
     @Parameter(property = "project.runtimeClasspathElements")
     private List<String> elements;
 
+    @Parameter(property = "project.build.outputDirectory")
+    private String outDir;
+
+    @Parameter(required = true)
+    private String executor;
+
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info(" classpath" + elements.toString());
-        URL[] runtimeUrls = new URL[elements.size()];
-        for (int i = 0; i < elements.size(); i++) {
-            String element = (String) elements.get(i);
-            try {
-                runtimeUrls[i] = new File(element).toURI().toURL();
-            } catch (MalformedURLException e) {
-                throw new MojoExecutionException("Failed to load project classpath", e);
-            }
-        }
-        URLClassLoader newLoader = new URLClassLoader(runtimeUrls,
-                Thread.currentThread().getContextClassLoader());
-        Reflections reflections = new Reflections(new ConfigurationBuilder().addClassLoader(newLoader).forPackages(packageName));
+
+        Thread.currentThread().setContextClassLoader(getClassLoader());
+        Reflections reflections = new Reflections(packageName);
         Set<Class<?>> migrations = reflections.getTypesAnnotatedWith(Migration.class);
-        getLog().info(migrations.toString());
+        List<Class<?>> list = new ArrayList<>(migrations);
+        Collections.sort(list, new Comparator<Class<?>>() {
+            @Override
+            public int compare(Class<?> o1, Class<?> o2) {
+                return o1.getSimpleName().compareTo(o2.getSimpleName());
+            }
+        });
+        try {
+            Executor executorImpl = (Executor) Thread.currentThread().getContextClassLoader().loadClass(executor).newInstance();
+            Options options = new Options();
+            executorImpl.execute(options, list);
+        } catch (Exception e) {
+            throw new MojoExecutionException("Failed to initialize dependencies.", e);
+        }
     }
+
+    private ClassLoader getClassLoader() throws MojoExecutionException {
+        elements.add(outDir);
+        try {
+            URL urls[] = new URL[elements.size()];
+
+            for (int i = 0; i < elements.size(); ++i) {
+                urls[i] = new File((String) elements.get(i)).toURI().toURL();
+            }
+            return new URLClassLoader(urls, getClass().getClassLoader());
+        } catch (Exception e)//gotta catch em all
+        {
+            throw new MojoExecutionException("Couldn't create a classloader.", e);
+        }
+    }
+
 }
