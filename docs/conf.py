e32b24cdeb34aad537569a494d0374171e701705
Liu Yang <liu_yang@worksap.co.jp>
2015-04-16 10:40:50 +0800

    Add graphviz

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,9 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.todo', 'sphinx.ext.graphviz']
+todo_include_todos = True
+graphviz_output_format = 'svg'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
